341b53d1432a3268f1cd6531958f4f421c925fee
Rayyan <rayyan.suhail2001@gmail.com>
2025-02-19 17:08:53 -0500

    Initial Commit

---
+++ DailyFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyFlow
+//
+//  Created by Rayyan Suhail on 2025-02-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyFlow/DailyFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyFlowApp.swift
+//  DailyFlow
+//
+//  Created by Rayyan Suhail on 2025-02-19.
+//
+
+import SwiftUI
+
+@main
+struct DailyFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
